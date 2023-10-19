45b28fb0d73b6de7c8962671bd3d161908f8c6cd
Filip Gutic <filipgutic88@gmail.com>
2023-10-19 13:05:11 +0200

    Infrastructure

---
+++ InvestchainApp/src/InvestchainApp.Application/Dto/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestchainApp.Application.Dto
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Currency, CurrencyDto>();
+        }
+    }
+}
